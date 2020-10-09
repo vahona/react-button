ab5f0bf4cb710e710fe3e79feb93b580950dd9d3
Belle Sugi Zafimarovololona <sugi.zaf@onja.org>
2020-10-09 10:36:26 +0300

    Create all button

--- script.js
+++ script.js
@@ -3,22 +3,20 @@ import ReactDOM from 'react-dom'
 import './style.css'
 
 
-
-
-
 class App extends React.Component{
 
-    constructor() {
-        super();
-        this.setState = { button: 'default'}
+    constructor(props) {
+        super(props);
+        
 
         this.handleClick = this.handleClick.bind(this)
         // this.handleHover = this.handleHover.bind(this)
     }
 
 
-    handleClick () {
-    
+    handleClick() {
+        e.preventDefault();
+     console.log("click me");
     }
 
 
@@ -26,33 +24,47 @@ class App extends React.Component{
    return (<div className = "list-button">
        <Button className="default"/>
        <Button className="gray" />
-       <Button className="white" />
+       <Button className="white"
+               variant="outline" />
        <Button className="light-blue" />
-       <Button className="no-border" />
+       <Button className="no-border"
+               variant="text" />
        <Button className="blue-no-border" />
-       <Button className="blue" />
-       <Button className="light-gray" />
-       <Button className="transparent" />
-       <Button className="left-icon" />
-       <Button className="right-icon" />
-       <Button className="small" />
-       <Button className="medium" />
-       <Button className = "big" />
-       <Button className = "gray-light2" />
-       <Button className = "blue2" />
-       <Button className = "black-light" />
-       <Button className = "red" />
+       <Button className="blue"
+              variant="disableShadow" />
+       <Button className="light-gray"
+               disabled />
+       <Button className="transparent"
+               variant="text" disabled />
+       <Button className="left-icon"
+               starticon="local_grocery_store"/>
+       <Button className="right-icon" 
+               endicon="local_grocery_store" />
+       <Button className="small"
+               size="sm"  />
+       <Button className="medium" 
+               size="md"  />
+       <Button className="big"
+               size="lg"  />
+       <Button className = "gray-light2" 
+               color="default"/>
+       <Button className = "blue2"
+               color="primary" />
+       <Button className = "black-light"
+               color="secondary" />
+       <Button className = "red"
+              color="danger" />
        <Button className = "gray-pink" />
        <Button className = "blue3" />
        <Button className = "black" />
        <Button className = "grenat" />
-
-     
    </div>)
 } 
 }
 
-function Button() {
+
+function Button(props) {
+  
     return (<div className="button-container">
         <button type="text" className="container-btn" >Default</button>
         </div>)
