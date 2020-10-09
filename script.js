8225d5dbdbd6ef633d2de5e78251a3dab7b45007
Belle Sugi Zafimarovololona <sugi.zaf@onja.org>
2020-10-09 16:29:57 +0300

    Create all button

--- script.js
+++ script.js
@@ -1,84 +1,69 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './style.css'
+import './style.css';
+
+
+
 
 
 class App extends React.Component{
 
     constructor(props) {
         super(props);
-        this.state = {
-            color : 'white'
-        }
-
-        this.handleClick = this.handleClick.bind(this)
-
+ 
     }
 
-    handleClick() {
-        this.setState({ color: 'red'})
 
-        console.log("click me");
-      
-    }
-
+   
 
     render() {
         
-        return (<div className="list-button" onClick={this.handleClick} >
+        return (<div className="list-button" >
 
-       <Button className="default" color="blue" / >
-       <Button className="gray" />
-       <Button className="white"
-               variant="outline" />
-       <Button className="light-blue" />
-       <Button className="no-border"
-               variant="text" />
-       <Button className="blue-no-border" />
-       <Button className="blue"
-              variant="disableShadow" />
-       <Button className="light-gray"
-               disabled />
-       <Button className="transparent"
-               variant="text" disabled />
-       <Button className="left-icon"
-               starticon="local_grocery_store"/>
-       <Button className="right-icon" 
-               endicon="local_grocery_store" />
-       <Button className="small"
-               size="sm"  />
-       <Button className="medium" 
-               size="md"  />
-       <Button className="big"
-               size="lg"  />
-       <Button className = "gray-light2" 
-               color="default"/>
-       <Button className = "blue2"
-               color="primary" />
-       <Button className = "black-light"
-               color="secondary" />
-       <Button className = "red"
-              color="danger" />
-       <Button className = "gray-pink" />
-       <Button className = "blue3" />
-       <Button className = "black" />
-       <Button className = "grenat" />
+       <Button />
+       <Button />
+       <Button variant="outline" />
+       <Button />
+       <Button variant="text" />
+       <Button />
+       <Button variant="disableShadow" />
+       <Button disabled />
+       <Button variant="text" disabled />
+       <Button starticon="local_grocery_store"/>
+       <Button endicon="local_grocery_store" />
+       <Button size="sm"  />
+       <Button size="md"  />
+       <Button size="lg"  />
+       <Button color="default"/>
+       <Button color="primary" />
+       <Button color="secondary" />
+       <Button color="danger" />
    </div>)
 } 
 }
 
 
-class Button extends React.Component {
-    constructor() {
-        super()
+
+function Button(props) {
+
+    let decorations = '';
+
+    if(props.color) {
+       decorations = props.color
+    }
+    if (props.size) {
+        decorations = props.size
     }
 
-  render() { 
+    if (props.variant){
+        decorations = props.variant
+    }
+
     return (<div className="button-container">
-        <button type="button" className="container-btn" > Default </button>
+        <button type="button" className={decorations}> Default </button>
         </div>)
 
-  }
+  
 }
 
 
